ef586fe3d5ddf2cb646e9cba3d99d48fbc87c50b
ProblemCh1ldren <95444614+ProblemCh1ldren@users.noreply.github.com>
2022-10-28 14:47:00 +0300

    Update Task_With_Pthread

--- Task_With_Pthreads.c
+++ Task_With_Pthreads.c
@@ -9,73 +9,75 @@
 #include <unistd.h>
 
 //Сумма делителей
-long int numDivisors = 0;
+long long int numDivisors = 0;
 pthread_mutex_t mutex;
 
 //Структура для потоковой функции
-struct interval_t
+typedef struct
 {
-  long int start;
-  long int end;
-  int flag;//Шаг
-};
+    long int start;
+    long int end;
+    int flag;//Шаг
+} interval_t;
 
 //Потоковая функция
 void* pthread_job(void* interval)
 {
-  struct interval_t* temp = (struct interval_t*) interval;
-  long int cores = sysconf(_SC_NPROCESSORS_CONF);
-  //Цикл нахождения делителей с шагом в количество ядер.
-  for (long int i = (temp -> start + temp -> flag); i <= temp -> end; i+= cores)
+    interval_t* temp = (interval_t*) interval;
+    long int cores = sysconf(_SC_NPROCESSORS_CONF);
+    //Цикл нахождения делителей с шагом в количество ядер.
+    for (long int i = (temp -> start + temp -> flag); i <= temp -> end; i+= cores)
     {
-      for (long int j = 1; j <= sqrt(i); j++)
-       {
-         if (!(i % j))
-          {
-            pthread_mutex_lock(&mutex);
-            if (j != i/j)
-              {
-                numDivisors += 2;
-              }
-            else
-              {
-                numDivisors++;
-              }
-            pthread_mutex_unlock(&mutex);
-          }
-      }
+        for (long int j = 1; j <= sqrt(i); j++)
+        {
+            if (!(i % j))
+            {
+                pthread_mutex_lock(&mutex);
+                if (j != i/j)
+                {
+                    numDivisors += 2;
+                }
+                else
+                {
+                    numDivisors++;
+                }
+                pthread_mutex_unlock(&mutex);
+            }
+        }
     }
-  return NULL;
- }
+    return NULL;
+}
 //Основная программа.
 int main(int argc, char* argv[])
 {
-  if (argc != 3)
-  {
-    printf("Введите левую и правую границу интервала\n");
-    return -1;
-  }
-  pthread_t *t;
-  struct interval_t* ThreadsStructs;
-  //Количество ядер компьютера, по совместительству количество потоков.
-  long int cores = sysconf(_SC_NPROCESSORS_CONF);
-  pthread_mutex_init(&mutex, NULL);
-  //Выделение памяти для количества потоков, ориентируется на количество ядер.
-  t = (pthread_t *) malloc(cores * sizeof(pthread_t));
-  ThreadsStructs = (struct interval_t *) malloc(cores * sizeof(struct interval_t));
-  for (int i=0; i < cores; i++)
+    if (argc != 3)
     {
-      ThreadsStructs[i].start = atoi(argv[1]);//atoi-из строки в число.
-      ThreadsStructs[i].end = atoi(argv[2]);
-      ThreadsStructs[i].flag = i;
-      pthread_create(&t[i],NULL,pthread_job,&ThreadsStructs[i]);
+        printf("Введите левую и правую границу интервала\n");
+        return -1;
     }
-  for(int i=0; i < cores ; i++)
+    pthread_t *t;
+    interval_t* ThreadsStructs;
+    //Количество ядер компьютера, по совместительству количество потоков.
+    long int cores = sysconf(_SC_NPROCESSORS_CONF);
+    pthread_mutex_init(&mutex, NULL);
+    //Выделение памяти для количества потоков, ориентируется на количество ядер.
+    t = (pthread_t *) malloc(cores * sizeof(pthread_t));
+    ThreadsStructs = (interval_t *) malloc(cores * sizeof(interval_t));
+    for (int i=0; i < cores; i++)
     {
-      pthread_join(t[i],NULL);
+        ThreadsStructs[i].start = atoi(argv[1]);//atoi-из строки в число.
+        ThreadsStructs[i].end = atoi(argv[2]);
+        ThreadsStructs[i].flag = i;
+        pthread_create(&t[i],NULL,pthread_job,&ThreadsStructs[i]);
     }
-  pthread_mutex_destroy(&mutex);
-  printf("Количесто делителей чисел из интервала: %ld\n",numDivisors);
-  printf("Количество ядер компьютера: %ld\n",cores);
-  return 0;
-  }
+    for(int i=0; i < cores ; i++)
+    {
+        pthread_join(t[i],NULL);
+    }
+    pthread_mutex_destroy(&mutex);
+    free(t);
+    free(ThreadsStructs);
+    printf("Количесто делителей чисел из интервала: %lld\n",numDivisors);
+    printf("Количество ядер компьютера: %ld\n",cores);
+    return 0;
+}
