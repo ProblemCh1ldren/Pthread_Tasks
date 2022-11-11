93e653133e052eace6718d90e519e3f83798d6a0
ProblemCh1ldren <95444614+ProblemCh1ldren@users.noreply.github.com>
2022-11-11 13:41:05 +0300

    Task with intervals on TCP

---
+++ TCP_client.c
@@ -0,0 +1,77 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct interval
+{
+    long int start;
+    long int end;
+} interval_t;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 4)
+    {
+        printf("Неверное количество аргументов\nВведите IP-адрес и две границы интервала\n");
+        return -1;
+    }
+    //структура для размещения адреса сервера
+    struct sockaddr_in servaddr;
+    int sockfd;
+    int n;
+    //создаем TCP-сокет
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror(NULL);
+        exit(1);
+    }
+    //перед заполнением обнуляем всю структуру
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET; //семейство
+    servaddr.sin_port = htons(51000); //хост
+
+    //преобразует адрес в нужную форму и сохраняет в поле структуры
+    if (inet_aton(argv[1], &servaddr.sin_addr) == 0)
+    {
+        printf("Неверный IP-адрес\n");
+        close(sockfd);
+        exit(1);
+    }
+    //устанавливаем соединение через созданный сокет с сокетом сервера, адрес которого в структуре
+    if (connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror(NULL);
+        close(sockfd);
+        exit(1);
+    }
+
+    long int cores = sysconf(_SC_NPROCESSORS_CONF);
+
+    interval_t* PthreadStructs;
+    PthreadStructs = (interval_t*)malloc(cores * sizeof(interval_t));
+
+    // write(sockfd, &count_of_cores, sizeof(count_of_cores));
+    //если левая граница = 1
+    for (int i = 0; i < cores; i++)
+    {
+        PthreadStructs[i].start = atoi(argv[2]) + i;
+        PthreadStructs[i].start = atoi(argv[3]);
+        if ((n = write(sockfd, &PthreadStructs[i], sizeof(PthreadStructs[i]))) < 0)
+        {
+            perror(NULL);
+            close(sockfd);
+            exit(1);
+        };
+    }
+
+    close(sockfd);
+    free(PthreadStructs);
+    return 0;
+}
